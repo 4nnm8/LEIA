6a4ea3aa76f12ebc16fb3cb91fe7cac5ca658252
Ann M <37158166+Loarg-Ann@users.noreply.github.com>
2018-03-10 18:38:13 +0100

    Add files via upload

--- leia.js
+++ leia.js
@@ -17,6 +17,8 @@ DONS : https://www.okpal.com/leia
 ////////// VARIABLES ///////////////////////////////////////////////////////////////////////////
   const dico = [
     ['[eè]([crt])','(èc)?(h)?e','$1e$3$7 $1è$3$6e$7','$1è$3$6e$7','$1e$3$7'],
+	['(eu)(r)','ice','$1$3$4$6 $1$4$5$6','$1$3$4$6','$1$3$4$6'],
+	['(eu)(r)','se','$1$3$4$6 $1$3$5$6','$1$3$5$6','$1$3$4$6'],
     ['ieux|ais|ous?|agnon|eaux?|(i|eu)[rnf]|e','esse|èche|outes?|rice|(?:[eo]|iei)lles?|[ai]gne|[oe]resse|(i|eu)[vs]e','$1$2$6 $1$4$6','$1$4$6','$1$2$6'],
     ['[eè][rt]e?|ieilles?|aîches?|agne|outes?|rice|[oe]lles?|[oe]resse|eu[sv]e|i(v|gn)e','agnon|ieux|[eè][rt]e?|os|ais|eau|[eo]u[rsf]?|i[nf]','$1$2$5 $1$4$5','$1$2$5','$1$4$5'],
     ['[aoe]ux','[ae]l{1,2}es|[eao]u[cs]{1,2}es?','$1$2 $1$3','$1$3','$1$2'],
@@ -31,30 +33,43 @@ DONS : https://www.okpal.com/leia
 	['grec','que[-·∙.•]?(s)?','grec$3','grecque$3','grec$3'],
     ['fra[iî]s?','(aî)?che[-·∙.•]?s?','frais fraîches','fraîches','frais'],
 	['héro(ïne)?s?','os|o?ïne·?(s)?','héros héroïne$4','héroïne$4','héros'],
-    ['(c)?(eux|elui)','elles?','$2$3 $2$4','$2$4','$2$3'],
-    ['(c)?(elles?)','eux|elui','$2$3 $2$4','$2$3','$2$4'],
-    ['cet?','t?te','$1 $1$2','$1$2','$1'],
+    ['(c)?(eux|elui)','elles?','$2$3 $2$4','$2$4','$2$3','$2$3 $2$4'],
+    ['(c)?(elles?)','eux|elui','$2$3 $2$4','$2$3','$2$4','$2$3 $2$4'],
+    ['cet?','t?te','$1 $1$2','$1$2','$1','$1 $2'],
     ['du|au|le','(de|à)? la','$1 $2','$2','$1'],
     ['(de|à)? la','du|au|le','$1 $3','$1','$2'],
     ['l[ea]','l?([ea])','$1 l$3','la','le']
   ];
   var dl = dico.length,	
-	  pl = pron.length,
-	  a = getParam('leia'),
-	  b = getParam('imode'),
-	  c = getParam('voice'),
+      pl = pron.length,
       rt = 1.2,
-	  ps = 0,
-	  imode, voice, nb,
-	  nodeList = [],
-	  tree = document.createTreeWalker(document.body,NodeFilter.SHOW_TEXT,{ 
-	    acceptNode: function(node) { 
-	      return NodeFilter.FILTER_ACCEPT; 
-	    } 
-	  },false);
+      ps = 0,
+      altgr = false,
+      leia,
+      imode,
+      voice,
+      nb,
+      nodeList = [],
+      tree = document.createTreeWalker(document.body,NodeFilter.SHOW_TEXT,{ 
+        acceptNode: function(node) { 
+	  return NodeFilter.FILTER_ACCEPT; 
+        } 
+      },false);
 
 ////////// FONCTIONS GÉNÉRALES /////////////////////////////////////////////////////////////////
 
+/** Vérification de la disponibilité du stockage des variables  **/
+  function storageAvailable(type) {
+    try {
+      var storage = window[type], x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    }
+    catch(e) {
+      return false;
+    }
+  }
 /** Génère des écouteurs d'évènements **/
 
   function addEvent(obj,evt,fn) {
@@ -94,28 +109,28 @@ DONS : https://www.okpal.com/leia
     document.getElementsByTagName('head')[0].appendChild(script)
   }
   
-/** Récupère les paramètres passés dans l'URL **/
-
-  function getParam(par){
-	var reg = new RegExp('.*[?&]'+par+'=([0-9]{1,2}).*','gi'),
-	    url = window.location.toString();
-	if (url.match(reg)){
-      return url.replace(reg,'$1');
-	} else {
-      return false; 
-	}
-  }
-  
 ////////// DÉBUT LÉIA //////////////////////////////////////////////////////////////////////////
 
-  if ((!a)||(a == 1)) {	init() }
+
+
+  if (storageAvailable('localStorage')) {
+    leia = localStorage.getItem("leia") || 1;
+    imode = localStorage.getItem("imode") || 2;
+    voice = localStorage.getItem("voice") || 0;
+  }
+  else {
+    console.log('Conservation des variables impossible')
+  }
+  if (leia == 1) {
+   init()
+  }
   
   // Si la synthèse vocale intégrée est activée...
-  if (c == 1) {
+  if (voice == 1) {
     voice = true;
 	// ... on charge ResponsiveVoice (http://code.responsivevoice.org/responsivevoice.js)
     loadScript('responsivevoice.js',function(){
-	  speak('La synthèse vocale est prête !')
+	  console.log('La synthèse vocale est prête !')
     });
   } else { 
     voice = false;
@@ -132,30 +147,21 @@ DONS : https://www.okpal.com/leia
   /** Initialisation - définis les paramètres et lance la conversion **/
 
   function init(){
-	if (b) {
-	  imode = Number(b) + 1;
-    } else {
-	  imode = 2;
-    }
     console.log('Mode de lecture n° '+(imode-1));
 	
 	// On lance le remplacement de l'écriture inclusive dans la page
 	skim(document.body);
 	
-	// On ajoute la prise en charge des raccourcis clavier (Alt + Shift + ...) vers les différentes fonctions
-    addEvent(document,'keydown',function(e){
-	  let k = e.which || e.keyCode || e.charCode;
-	  if (((e.altKey && e.ctrlKey) || e.altLeft)&&(k==59 || k==190 || k==186 || k==110)){  // 225 linux
-	    middot();
-		e.preventDefault();
-	  }
-      if (e.altKey && e.shiftKey){
+  // On ajoute la prise en charge des raccourcis clavier (Alt + Shift + ...) vers les différentes fonctions
+  addEvent(document,'keydown',function(e){
+    let k = e.which || e.keyCode || e.charCode;
+    if (e.altKey && e.shiftKey){
 	    switch (k) {
           case 38: read('u'); break;  // [up arrow]
 		  case 40: read('d'); break;  // [down arrow]	   
 	      case 67: leiaconf(); break; // [C]
-	      case 107 : rt = Math.round((rt+0.2)*10)/10; speak('Débit augmentée à '+rt);break; //!\ [+] Vérifier la compatibilité cross-browser
-	      case 109 : rt = Math.round((rt-0.2)*10)/10; speak('Débit diminuée à '+rt);break;  //!\ [-] Vérifier la compatibilité cross-browser
+	      case 107: rt = Math.round((rt+0.2)*10)/10; speak('Débit augmentée à '+rt);break;
+		  case 109: rt = Math.round((rt-0.2)*10)/10; speak('Débit diminuée à '+rt);break;
         }
         e.preventDefault();
       }
@@ -164,8 +170,6 @@ DONS : https://www.okpal.com/leia
   
 /** Fonction de convertion récursive des mots **/
   function skim(elem){
-
-
     while(tree.nextNode()){
 	  if (tree.currentNode.nodeValue.trim().length > 0){
 		nodeList.push(tree.currentNode);
@@ -239,35 +243,55 @@ DONS : https://www.okpal.com/leia
         input.selectionEnd = startPos + text.length; 
 	  } else {
         input.value += text;
-      }
+    }
+
   } 
 
-/** FONCTION D'ORALISATION **/
+  addEvent(document,'keyup',function(e){
+    if ((e.which || e.keyCode || e.charCode) == 225){
+      altgr=false;
+    }
+  });
+  document.querySelectorAll('textarea,input[type=text],input[type=password],input[type=date],input[type=number]').forEach(function(elem){
+    addEvent(elem,'keydown',function(e){
+      let k = e.which || e.keyCode || e.charCode,
+          kp = [59,190,186,110];
+      if (k==225) {
+        altgr=true;
+      }
+      if (altgr === true && (kp.includes(k))) {
+        middot();
+        e.preventDefault();
+      }
+	    if (((e.altKey && e.ctrlKey) || e.altLeft)&&(kp.includes(k))){
+	      middot();
+	      e.preventDefault();
+	    }
+    });
+    if (voice) {
+      addEvent(elem,'keypress',function(e){
+        speak(String.fromCharCode(e.which || e.keyCode || e.charCode));
+	      setTimeout(function(){
+	        if (elem.value.match(/([-∙'".•,\s\]\)\/]$)/)){
+            var y = (elem.value.match(/\b([a-zâäàçéèêëïîôöùûü·∙•.'-]+[-·∙"'•.,\s\]\)\/])$/gi))[0];
+            for (var i = 0; i < dl; i++) {
+	            var x = new RegExp('([^\f\n\r\t\v​\. -]+)('+dico[i][0]+')[-·.•∙]('+dico[i][1]+')[-·.•∙]?(s)?','gi');
+			        y = y.replace(x,dico[i][imode])
+            } 
+		        speak(y);
+	        }
+	      },800);
+      });
+	  }
+  });
   function speak(s){
     if ((typeof responsiveVoice != 'undefined') && (responsiveVoice.voiceSupport()) && (!!s)) {
       responsiveVoice.speak(s,'French Female',{rate:rt}) // onend: EndCallback
     } else {
-	  console.log('ResponsiveVoice n\'est pas activé.')
-	}
+	    console.log('ResponsiveVoice n\'est pas activé.')
+	  }
   }
 
-  if (voice) {
-    document.querySelectorAll('textarea,input[type=text],input[type=password],input[type=date],input[type=number]').forEach(function(elem){
-      addEvent(elem,'keypress',function(event){
-	    speak(String.fromCharCode(event.which||event.key));
-	    setTimeout(function(){
-	      if (elem.value.match(/([-·•.,\s\]\)\/]$)/)){
-            var y = (elem.value.match(/\b([a-zâäàçéèêëïîôöùûü·∙•.'-]+[-·•.,\s\]\)\/])$/gi))[0];
-            for (var i = 0; i < dl; i++) {
-	          var x = new RegExp('([^\f\n\r\t\v​\. -]+)('+dico[i][0]+')[-·.•]('+dico[i][1]+')[-·.•]?(s)?','gi');
-			      y = y.replace(x,dico[i][imode])
-            } 
-		    speak(y);
-	      }
-	    },400)
-	  });
-    });
-  }
   
 })(); /** FIN DU SCRIPT */
 
