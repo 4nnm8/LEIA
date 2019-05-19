0cdd6349833306a7fbed00e77c75a1c7b4be0281
Ann MB <37158166+ANN-MB@users.noreply.github.com>
2019-05-19 16:22:05 +0200

    Add files via upload

--- extension firefox/background.js
+++ extension firefox/background.js
@@ -1,20 +1,11 @@
-var leia = {
-  mode: 1,
-  pred: 0,
-  high: 0,
-  styl: ''
-}
-function onError(e) {
-  console.error(e);
-}
-function checkStoredSettings(storedSettings) {
-  if (!storedSettings.leia) {
+var leia = {mode:1, pred:0, high:0, styl:"emph9"}
+browser.storage.local.get().then(function(stored){
+  if (!stored.leia) {
     browser.storage.local.set({leia});
   }
-}
-var gettingStoredSettings = browser.storage.local.get();
-gettingStoredSettings.then(checkStoredSettings, onError);
-
-function handleClick() { browser.runtime.openOptionsPage(); }
-browser.browserAction.onClicked.addListener(handleClick);
-browser.runtime.onMessage.addListener(handleClick);
+}, function(e){
+	console.error(e);
+});
+browser.browserAction.onClicked.addListener(function(){
+	browser.runtime.openOptionsPage();
+});
