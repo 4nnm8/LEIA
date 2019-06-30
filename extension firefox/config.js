407fd715cf618fabac32a9629d9e42c32031233c
Ann MB <37158166+ANN-MB@users.noreply.github.com>
2019-06-30 22:26:30 +0200

    v 2.8

--- extension firefox/config.js
+++ extension firefox/config.js
@@ -4,7 +4,8 @@ var modeipt = document.getElementById("mode"),
     stylipt = document.getElementById("styl"),
 	semiipt = document.getElementById("semi"),
     example = document.getElementById("ex"),
-    buttons = document.getElementById("buttons");
+    buttons = document.getElementById("buttons"),
+	bradios = document.getElementsByName("emph-styles");
 
 browser.storage.local.get().then(function(a) {
   modeipt.selectedIndex = a.leia.mode;
@@ -12,11 +13,13 @@ browser.storage.local.get().then(function(a) {
   highipt.selectedIndex = a.leia.high;
   a.leia.semi == 1 ? semiipt.checked = true : semiipt.checked = false;
   stylipt.value = a.leia.styl;
-  1 == a.leia.high ? (example.className = a.leia.styl, buttons.style.visibility = "visible") : buttons.style.visibility = "hidden";
+  1 == a.leia.high ? (example.className = a.leia.styl, buttons.style.display = "block") : buttons.style.display = "none";
 }, function(a) {
   console.error(a);
 });
+
 function storeSettings() {
+	console.log('changed')
   var modeval = modeipt.options[modeipt.selectedIndex].value,
       predval = predipt.options[predipt.selectedIndex].value,
       highval = highipt.options[highipt.selectedIndex].value,
@@ -32,22 +35,22 @@ function storeSettings() {
     }
   });
 }
-buttons.addEventListener("click", function(e) {
+
+document.getElementById("form").addEventListener("change", function(e) {
   e.preventDefault();
-  "BUTTON" == e.target.tagName && (
-    newstyl = "emph"+e.target.name,
-    example.className = stylipt.value = newstyl,
-    storeSettings()
-  );
-});
-document.getElementById("form").addEventListener("change",function(e) { 
+  for (var i = 0; i < bradios.length; i++) {
+    bradios[i].checked && (
+	  example.className = bradios[i].id,
+	  stylipt.value = bradios[i].id
+    );
+  }
   switch (e.target.id) {
     case "mode":
-      0 !== modeipt.selectedIndex && (highipt.selectedIndex = "0", buttons.style.visibility = "hidden");
+      0 !== modeipt.selectedIndex && (highipt.selectedIndex = "0", buttons.style.display = "none");
     break;
     case "high":
-     (1 == highipt.selectedIndex) ? (modeipt.selectedIndex = "0", buttons.style.visibility = "visible") : buttons.style.visibility = "hidden";
+     (1 == highipt.selectedIndex) ? (modeipt.selectedIndex = "0", buttons.style.display = "block", document.getElementById("emph1").focus()) : buttons.style.display = "none";
     break;
   }
   storeSettings(); 
-});
+},false);
