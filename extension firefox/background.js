72280a7d496927a0558e60c2f15f7d61a12c73e9
Ann MB <37158166+ANN-MB@users.noreply.github.com>
2019-07-03 15:56:15 +0200

    Delete background.js

--- extension firefox/background.js
+++
@@ -1,7 +0,0 @@
-var leia = {mode:1,pred:0,high:0,styl:"emph4"}
-browser.storage.local.get().then(function(s){
-  !s.leia && browser.storage.local.set({leia});
-}, function(e){
-  console.error(e);
-});
-browser.browserAction.onClicked.addListener(function(){browser.runtime.openOptionsPage()});
